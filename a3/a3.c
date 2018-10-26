ae1407d8759b7cbfc2ce5cec7a6b9499915a0d81
Jonah <jkubath1@gmail.com>
2018-10-25 20:15:38 -0400

    A3

--- a3/a3.c
+++ a3/a3.c
@@ -1,22 +1,32 @@
 #include "a3.h"
 
-// struct timespec {
-//     time_t tv_sec;      /* Seconds */
-//     long   tv_nsec;     /* Nanoseconds [0 .. 999999999] */
-// };
-
 struct timespec ts;
 
 
 int main(int argc, char ** argv) {
-  ts.tv_sec = 1;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  ts.tv_sec += 1;
+
 	sem_t * sems;
 	sem_t * semk;
-	sems = sem_open("screen", O_CREAT);
-	semk = sem_open("keyboard", O_CREAT);
-	int pids[9]; 
+  sem_unlink("screen");
+  sem_unlink("keybaord");
+	sems = sem_open("screen", O_CREAT|O_EXCL, 0644, 0);
+  if(errno == SEM_FAILED) {
+    printf("Screen semaphore failed to open\n");
+    exit(1);
+  }
+	semk = sem_open("keyboard", O_CREAT|O_EXCL, 0644, 0);
+  if(errno == SEM_FAILED) {
+    printf("Keyboard semaphore failed to open\n");
+    exit(1);
+  }
+
+  int procNumber = 1;
+	int pids[procNumber]; 
 	int pid; // Holds the PID of the forked process
 	int i = 0; // Index value
+  
 
   /* Open 9 processes */
 	do {
@@ -31,12 +41,14 @@ int main(int argc, char ** argv) {
     }
 
 		i++;
-	} while( i < 9 && pid > 0);
+	} while( i < procNumber && pid > 0);
 	
   //wait for all children to finish
-	if(i >= 9 && pid != 0) {
-    for(i = 0; i < 9; i++) {
+	if(i >= procNumber && pid != 0) {
+    i = 0;
+    while(i < procNumber) {
       waitpid(pids[i], NULL, 0);
+      i++;
     }
 
     // sem_unlink both 
@@ -53,6 +65,8 @@ int main(int argc, char ** argv) {
 void Process(sem_t * screen, sem_t * keyboard, int index) {
   int count = 0; // Count of deadlocks for this process
   char * input = calloc(80, 1);
+
+  printf("Process %d running\n", index);
 	
   while(strnlen(input, 80) > 1 || input[0] != 'q') {
     count += semaphores(screen, keyboard, index);
@@ -61,66 +75,77 @@ void Process(sem_t * screen, sem_t * keyboard, int index) {
     scanf("%s\n", input);
     printf("%d Input: %s\n", index, input);
 
-
-  //  	   prompt "enter < 80 characters or q to quit: "
-  //  	   read keboard
-  //  	   echo
-	//    sem_post or give back both semaphores
   }
 
+  sem_post(screen);
+  sem_post(keyboard);
 	// until quit end inner loop in C do ... while not q
 	//    count += getSemaphores(index)
 	//      prompt "This process had " + count + " deadlocks "
 	//    sem_post or give back both semaphores
 	// sem_close 
-	printf("Process %d finished\n", index);
+  sem_close(screen);
+  sem_close(keyboard);
+	printf("Process %d finished with %d deadlocks\n", index, count);
 
 
 }
 
-int semaphores (sem_t * screen, sem_t * keyboard, int index) {
+int semaphores(sem_t * screen, sem_t * keyboard, int index) {
   //returns count of how many times recovered from deadlock
   //odd index gets screen first
   //even index gets keyboard first
   int count = 0;
   int s = -1;
   int k = -1;
+  printf("Semaphores %d\n", index);
 
   // Even
-  if(index % 2) {
+  if((index % 2) == 0) {
+    printf("even\n");
     k = sem_wait(keyboard);
     printf("%d locked keyboard\n", index);
   }
   // Odd
   else {
+    printf("odd\n");
     s = sem_wait(screen);
     printf("%d locked screen\n", index);
   }
   	
   // Loop until both semaphores are locked
-  do {
+  while(s != 0 || k != 0) {
+    printf("%d while\n", index);
     // Even
-    if(index % 2) {
-      s = sem_timedwait(screen, ts);
-      printf("%d locked screen\n", index);
+    if((index % 2) == 0) {
+      s = sem_timedwait(screen, &ts);
+      if(s != 0) {
+        printf("%d failed to lock screen\n", index);
+        sem_post(keyboard);
+        sleep(1);
+        count++;
+      }
+      else {
+        printf("%d locked screen\n", index);
+      }
     }
     // Odd
     else {
-      k = sem_timedwait(keyboard, ts);
-      printf("%d locked keyboard\n", index);
+      k = sem_timedwait(keyboard, &ts);
+      if(s != 0) {
+        printf("%d failed to lock keyboard\n", index);
+        sem_post(screen);
+        sleep(1);
+        count++;
+      }
+      else {
+        printf("%d locked keyboard\n", index);
+      }
     }
 
   }
-  while(s != 0 || k != 0);
-  // 	loop
-  // 	sem_timedwait with timer for second
-  //  	if timeout 
-  //    		give back first, (sem_post)
-  //    		wait random time
-		// increment count
-  //  	else
-  //    		have both  true   
- 	// 		until have both
+  
+
  	return count;
 } //end get semaphores function
 
--- a3/a3.h
+++ a3/a3.h
@@ -5,6 +5,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 void Process (sem_t * screen, sem_t * keyboard, int index);
 int semaphores (sem_t * screen, sem_t * keyboard, int index);
